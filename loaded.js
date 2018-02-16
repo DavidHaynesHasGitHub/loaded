dd512b76a238ad06de5ddd348aaf8a4345feaf75
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-15 20:00:00 -0800

    completed the loaded die and blastoff

---
+++ loaded.js
@@ -0,0 +1,16 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var cheat = -1;
+
+  return function() {
+    cheat += 1;
+    return list[cheat];
+  };
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
